7ba781d24baa98b176dd18534646134d83914913
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-17 17:37:41 -0700

    added store functions to store.js, modified html views

---
+++ scripts/store.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const store = (function() {
+
+  const addItem = function(item) {
+    this.items.push(item);
+  };
+
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
+
+  // test data
+  const data = [
+    {
+      'id': '8sdfbvbs65sd',
+      'title': 'Google',
+      'url': 'http://google.com',
+      'desc': 'An indie search engine startup',
+      'rating': 4
+    },
+    {
+      'id': '87fn36vd9djd',
+      'title': 'Fluffiest Cats in the World',
+      'url': 'http://medium.com/bloggerx/fluffiest-cats-334',
+      'desc': 'The only list of fluffy cats online',
+      'rating': 5
+    }
+  ];
+  
+
+  return {
+    items: [],
+    error: null,
+
+    addItem,
+    findById,
+    findAndDelete
+  };
+  
+}());
