3f5637b75423f97c7530d580eb130cded129d8f4
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-17 17:43:35 -0700

    added api functions

---
+++ scripts/api.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const api = (function() {
+  
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mikelr';
+
+  const getItems = function(callback) {
+    $.getJSON(BASE_URL + '/bookmarks', callback);
+  };
+
+  const createItem = function(id, title, url, desc, rating, onSuccess, onError) {
+    console.log('was clicked');
+    const newItem = JSON.stringify(
+      {'id': id,
+        'title': title,
+        'url': url,
+        'desc': desc,
+        'rating': rating
+      });
+      console.log(newItem);
+
+    $.ajax({
+      url: BASE_URL + '/bookmarks',
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: onSuccess,
+      error: onError,
+    });
+  };
+
+  const updateItem = function(id, updateData, callback) {
+    $.ajax({
+      url: BASE_URL + '/bookmarks/' + id,
+      method: 'PATCH',
+      contentType: 'application/json',
+      data: JSON.stringify(updateData),
+      success: callback
+    });
+  };
+
+  const deleteItem = function(id, callback) {
+    $.ajax({
+      url: BASE_URL + '/bookmarks/' + id,
+      method: 'DELETE',
+      success: callback
+    });
+  };
+
+  return {
+    getItems,
+    createItem,
+    updateItem,
+    deleteItem,
+  };
+}());
---
+++ scripts/bookmark-list.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const bookmarkList = (function() {
+
+  function generateExpandedView(item){
+    `</li> 
+    
+    
+    
+    </li>`
+    render();
+  }
+
+
+  function createBookmark() {
+
+  }
+
+  function handleDeleteBookmark() {
+
+  }
+
+  function handleExpandView() {
+    $('') click event =>
+
+  }
+
+  function render() {
+
+  }
+
+  function main() {
+    handleExpandView();
+    handleDeleteBookmark();
+    createBookmark();
+  }
+
+
+}());
